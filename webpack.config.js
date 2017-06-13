c4925628eb742f0020dd8841fee56b3c16f6f79f
ifree92 <ifree92@gmail.com>
2017-06-13 18:24:32 +0300

    refactor: refactored webpack/package.json scheme for ability to develop on any platform (particularly Windows); update: updated node-sass to newer version because 4.5.0 had a problem with compilation on windows platform;

--- webpack.config.js
+++ webpack.config.js
@@ -8,50 +8,51 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const package = require('./package.json');
 const distPath = path.resolve(__dirname, 'dist');
 const appPath = path.resolve(__dirname, 'src/app');
-// Configs
-let config = {
-    entry: {
-        'app': appPath + '/app.js'
-    },
-    output: {
-        path: distPath,
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            { test: /\.jsx?/, use: 'babel-loader', include: appPath },
-            { test: /\.(png|woff|woff2|eot|ttf|svg|gif)/, use: 'url-loader?limit=100000' },
-            { test: /\.css$/, use: ["style-loader", "css-loader"] },
-            { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: package.title,
-            company: package.company,
-            description: package.description,
-            url: package.url,
-            filename: 'index.html',
-            template: 'src/assets/templates/index.ejs',
-            hash: true,
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true,
-                minifyJS: true
-            }
-        }),
-        new CopyWebpackPlugin([
-            {context: 'src', from: 'assets/img/fav/*'},
-            {context: 'src', from: 'assets/img/banner/*'}
-        ]),
-        new webpack.DefinePlugin({
-            VERSION: JSON.stringify(package.version),
-            ENV: JSON.stringify(process.env.ENV)
-        })
-    ],
-    devServer: {
-        historyApiFallback: true
+
+// Exports
+module.exports = function webpackStuff(env) {
+    return {
+        entry: {
+            'app': appPath + '/app.js'
+        },
+        output: {
+            path: distPath,
+            filename: '[name].js'
+        },
+        module: {
+            loaders: [
+                { test: /\.jsx?/, use: 'babel-loader', include: appPath },
+                { test: /\.(png|woff|woff2|eot|ttf|svg|gif)/, use: 'url-loader?limit=100000' },
+                { test: /\.css$/, use: ["style-loader", "css-loader"] },
+                { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: package.title,
+                company: package.company,
+                description: package.description,
+                url: package.url,
+                filename: 'index.html',
+                template: 'src/assets/templates/index.ejs',
+                hash: true,
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    minifyJS: true
+                }
+            }),
+            new CopyWebpackPlugin([
+                {context: 'src', from: 'assets/img/fav/*'},
+                {context: 'src', from: 'assets/img/banner/*'}
+            ]),
+            new webpack.DefinePlugin({
+                VERSION: JSON.stringify(package.version),
+                ENV: JSON.stringify(env)
+            })
+        ],
+        devServer: {
+            historyApiFallback: true
+        }
     }
 }
-// Exports
-module.exports = config;
